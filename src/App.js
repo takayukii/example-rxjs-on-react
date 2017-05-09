85977d67d1910a56063db6856d00babc60aa59c4
takayukii <takayukii@gmail.com>
2017-05-09 17:27:32 +0900

    Try takeUntil with click event

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// http://stackoverflow.com/questions/37316156/how-to-debounce-user-input-in-reactjs-using-rxjs
+
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
@@ -6,23 +8,27 @@ import Rx from 'rxjs';
 import {receiveGreeting} from './redux-things';
 import {apiIncrementalGreeting} from './api';
 
-// http://stackoverflow.com/questions/37316156/how-to-debounce-user-input-in-reactjs-using-rxjs
-
+let counter = 0;
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleClickMe = this.handleClickMe.bind(this);
+    this.handleGenerateGreeting = this.handleGenerateGreeting.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
 
+  componentDidMount() {
+    this.cancelButtonClicks = Rx.Observable.fromEvent(document.getElementById('cancel-button'), 'click');
     this.rxSubject = new Rx.Subject()
       .debounceTime(1000)
-      .scan(acc => ++acc, 0)
       .switchMap(params =>
         apiIncrementalGreeting(params)
           .map(res => res.greeting)
+          .do(this.props.receiveGreeting)
+          .takeUntil(this.cancelButtonClicks)
       );
 
     this.rxSubject.subscribe(greeting => {
-      this.props.receiveGreeting(greeting);
+      console.log('subscribe', greeting);
     });
   }
 
@@ -31,16 +37,20 @@ class App extends Component {
       <div className="container">
         <div className="jumbotron">
           <h3>{this.props.greeting}</h3>
-          <button onClick={this.handleClickMe}>Generate super greeting</button>
-          <button>Cancel it!</button>
+          <button onClick={this.handleGenerateGreeting}>Generate super greeting</button>
+          <button id="cancel-button" onClick={this.handleCancel}>Cancel it!</button>
         </div>
       </div>
     );
   }
 
-  handleClickMe(e) {
-    console.log('handleClickMe');
-    this.rxSubject.next(e);
+  handleGenerateGreeting(e) {
+    console.log('handleGenerateGreeting');
+    this.rxSubject.next(++counter);
+  }
+
+  handleCancel(e) {
+    console.log('handleCancel');
   }
 }
 
